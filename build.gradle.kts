2f39c244c4511e7135eae71b0960469f6ad806bc
chaikovskyiyaroslav-creator <chaikovskyi.yaroslav@e-u.edu.ua>
2026-04-04 00:20:27 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,28 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    alias(libs.plugins.android.application) apply false
+    id("com.android.application") version "8.1.0" apply true
+    id("org.jetbrains.kotlin.android") version "1.8.10" apply true
+}
+
+android {
+    namespace = "com.example.pra6"
+    compileSdk = 33
+
+    defaultConfig {
+        applicationId = "com.example.pra6"
+        minSdk = 24
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+        }
+    }
+    
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
 }
